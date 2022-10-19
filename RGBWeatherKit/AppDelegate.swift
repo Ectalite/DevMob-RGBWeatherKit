66cc93f30b96e5f95a6b9005f491fecbe9a97ef9
Xavier Hueber <xavier.hueber@laposte.net>
2022-10-19 13:34:33 +0200

    Cocoapod and App Center CI/CD Support

--- RGBWeatherKit/AppDelegate.swift
+++ RGBWeatherKit/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AppCenter.start(withAppSecret: "1b287a30-0c3d-4388-94d7-74ffc5c0e849", services:[
+          Analytics.self,
+          Crashes.self
+        ])
         return true
     }
 
@@ -30,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
