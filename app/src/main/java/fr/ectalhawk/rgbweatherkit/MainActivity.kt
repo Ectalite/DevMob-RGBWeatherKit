b263388a9e0672f67c85da097a2d06ae404df9d8
CybrHawk <accounts@foxtrothawk.com>
2022-11-01 16:37:08 +0100

    New startup activity and main menu activity

--- app/src/main/java/fr/ectalhawk/rgbweatherkit/MainActivity.kt
+++ app/src/main/java/fr/ectalhawk/rgbweatherkit/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_selection)
+        setContentView(R.layout.activity_menu_principal)
     }
 
     override fun onStart() {
