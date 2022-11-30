d52ef6e3d618cb746c3210d6cd854882d81cfa77
Xavier Hueber <xavier.hueber@he-arc.ch>
2022-11-30 11:11:32 +0100

    Test de l'API openweathermap. Spoiler: elle fonctionne du tonner !

---
+++ app/src/main/java/fr/ectalhawk/rgbweatherkit/weatherAPI/WeatherAPI.kt
@@ -0,0 +1,11 @@
+package fr.ectalhawk.rgbweatherkit.weatherAPI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class weatherAPI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_api)
+    }
+}
---
+++ app/src/main/java/fr/ectalhawk/rgbweatherkit/weatherAPI/respository/WeatherRepository.kt
@@ -0,0 +1,38 @@
+package com.example.myweather.respository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.myweather.api.MyWeatherServiceInterface
+import com.example.myweather.dataModels.MyWeather
+import com.example.myweather.utils.NetworkUtils
+import java.util.*
+
+//Repository is used to manage our data. It needs access to retrofit service to do so.
+class WeatherRepository(
+    private val myWeatherService: MyWeatherServiceInterface,
+    private val applicationContext: Context
+) {
+
+    private val weatherLiveData = MutableLiveData<Response<MyWeather>>()
+    //Will be called in MainViewModel.
+    val weather: LiveData<Response<MyWeather>>
+        get() = weatherLiveData
+
+    suspend fun getWeather(location: String, units: String) {
+        if(NetworkUtils.isInternetAvailable(applicationContext)){
+                val result = myWeatherService.getWeather(location,units, Locale.getDefault().language)
+                if(result.body() != null){
+                    weatherLiveData.postValue(Response.Success(result.body()))
+                }
+                else{
+                    weatherLiveData.postValue(Response.Error("Error 404: Location not found"))
+                }
+        }
+        else{
+            //handle no internet
+            weatherLiveData.postValue(Response.Error("No Internet"))
+        }
+
+    }
+}
